7859f1525db59ce59df5a18199c97433efd7256d
Rimsky <Rimsky@DESKTOP-BO2IQFC.lan>
2023-04-09 21:54:38 -0400

    "This is becoming easier"

---
+++ src/package5/Class5HW.java
@@ -0,0 +1,15 @@
+package package5;
+
+public class Class5HW {
+	
+	public static void main (String args []) {
+		System.out.println("Homework is almost done");
+	}
+	
+	public void multiply2 (int a, int b) {
+		System.out.println(a * b);
+	}
+	
+	
+
+}
